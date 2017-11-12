4bfe3700822ec0b6af0fc376dd5bc56f1cea94c5
José Carlos Chávez <jcchavezs@gmail.com>
2017-11-12 11:35:37 +0100

    Adjustes the model. This adjustments are based on http://zipkin.io/zipkin-api/\#/default/post_spans

--- model.go
+++ model.go
@@ -3,26 +3,27 @@ package zipkinagent
 type Endpoint struct {
 	ServiceName string `json:"serviceName,omitempty"`
 	IPv4        string `json:"ipv4,omitempty"`
+	IPv6        string `json:"ipv6,omitempty"`
 	Port        int    `json:"port,omitempty"`
 }
 
-type Log struct {
+type Annotation struct {
 	Timestamp int64  `json:"timestamp,omitempty"`
 	Value     string `json:"value,omitempty"`
 }
 
 type Span struct {
-	Kind            string            `json:"kind,omitempty"`
-	TraceID         string            `json:"traceId,omitempty"`
-	ParentID        string            `json:"parentId,omitempty"`
-	ID              string            `json:"id,omitempty"`
-	Name            string            `json:"name,omitempty"`
-	Timestamp       int64             `json:"timestamp,omitempty"`
-	TimestampMicros int64             `json:"timestampMicros,omitempty"`
-	Duration        int64             `json:"duration,omitempty"`
-	DurationMicros  int64             `json:"durationMicros,omitempty"`
-	LocalEndpoint   *Endpoint         `json:"localEndpoint,omitempty"`
-	RemoteEndpoint  *Endpoint         `json:"remoteEndpoint,omitempty"`
-	Tags            map[string]string `json:"tags,omitempty"`
-	Logs            []Log             `json:"annotations,omitempty"`
+	TraceID        string            `json:"traceId"`
+	Name           string            `json:"name,omitempty"`
+	ParentID       string            `json:"parentId,omitempty"`
+	ID             string            `json:"id"`
+	Kind           string            `json:"kind,omitempty"`
+	Timestamp      int64             `json:"timestamp,omitempty"`
+	Duration       int64             `json:"duration,omitempty"`
+	Debug          bool              `json:"debug"`
+	Shared         bool              `json:"shared"`
+	LocalEndpoint  *Endpoint         `json:"localEndpoint,omitempty"`
+	RemoteEndpoint *Endpoint         `json:"remoteEndpoint,omitempty"`
+	Annotations    []Annotation      `json:"annotations,omitempty"`
+	Tags           map[string]string `json:"tags,omitempty"`
 }
